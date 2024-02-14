8e93e6fc40c93aebb0f8c7b8a081aad0e6a92365
Alvin Philips <alvinphilips257@gmail.com>
2024-02-13 23:54:10 -0500

    Added Singleton pattern

---
+++ Assets/Game/Scripts/Patterns/Singleton.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public abstract class Singleton<T> : MonoBehaviour where T: Component
+{
+    private static T _instance;
+    public static T Instance {
+        get
+        {
+            _instance ??= FindAnyObjectByType<T>();
+
+            if (_instance != null) return _instance;
+            var go = new GameObject(typeof(T).Name);
+            _instance = go.AddComponent<T>();
+
+            return _instance;
+        }
+    }
+
+    public static bool HasInstance()
+    {
+        return _instance != null;
+    }
+
+    public void Awake()
+    {
+        if (_instance == null)
+        {
+            _instance = this as T;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
