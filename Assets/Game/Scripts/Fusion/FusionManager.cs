4b0bc9ee2f8cd51dcf1d6d5a297713f76e6f2f9b
Alvin Philips <alvinphilips257@gmail.com>
2024-02-15 22:24:02 -0500

    Namescaped FusionManager

--- Assets/Game/Scripts/Fusion/FusionManager.cs
+++ Assets/Game/Scripts/Fusion/FusionManager.cs
@@ -1,199 +1,203 @@
-using UnityEngine;
+using System;
+using System.Collections.Generic;
 using Fusion;
 using Fusion.Sockets;
-using System.Collections.Generic;
-using System;
+using Game.Scripts.Patterns;
+using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class FusionManager : MonoBehaviour, INetworkRunnerCallbacks
+namespace Game.Scripts.Fusion
 {
-    public NetworkRunner _runner;
-
-    [SerializeField] private NetworkPrefabRef _playerPrefab;
-    private Dictionary<PlayerRef, NetworkObject> _spawnedCharacters = new Dictionary<PlayerRef, NetworkObject>();
-
-
-    //Bind this to UI Buttons
-    public void CreateGame()
+    public class FusionManager : Singleton<FusionManager>, INetworkRunnerCallbacks
     {
-        StartGame(GameMode.Host);
-    }
+        public NetworkRunner _runner;
 
-    public void JoinGame()
-    {
-        StartGame(GameMode.Client);
-    }
+        [SerializeField] private NetworkPrefabRef _playerPrefab;
+        private Dictionary<PlayerRef, NetworkObject> _spawnedCharacters = new Dictionary<PlayerRef, NetworkObject>();
 
-    public void LeaveGame()
-    {
-        if(_runner != null)
+
+        //Bind this to UI Buttons
+        public void CreateGame()
         {
-            _runner.Shutdown();
+            StartGame(GameMode.Host);
         }
-    }
 
-    private void OnGUI()
-    {
-        if (_runner == null)
+        public void JoinGame()
         {
-            if (GUI.Button(new Rect(0, 0, 200, 40), "Host"))
+            StartGame(GameMode.Client);
+        }
+
+        public void LeaveGame()
+        {
+            if(_runner != null)
             {
-                CreateGame();
-            }
-            if (GUI.Button(new Rect(0, 40, 200, 40), "Join"))
-            {
-                JoinGame();
+                _runner.Shutdown();
             }
         }
-        else
+
+        private void OnGUI()
         {
-            if (GUI.Button(new Rect(0, 0, 200, 40), "Leave"))
+            if (_runner == null)
             {
-                LeaveGame();
+                if (GUI.Button(new Rect(0, 0, 200, 40), "Host"))
+                {
+                    CreateGame();
+                }
+                if (GUI.Button(new Rect(0, 40, 200, 40), "Join"))
+                {
+                    JoinGame();
+                }
+            }
+            else
+            {
+                if (GUI.Button(new Rect(0, 0, 200, 40), "Leave"))
+                {
+                    LeaveGame();
+                }
             }
         }
-    }
 
-    async void StartGame(GameMode mode)
-    {
-        // Create the Fusion runner and let it know that we will be providing user input
-        _runner = gameObject.AddComponent<NetworkRunner>();
-        _runner.ProvideInput = true;
-
-        // Create the NetworkSceneInfo from the current scene
-        var scene = SceneRef.FromIndex(SceneManager.GetActiveScene().buildIndex);
-        //var sceneInfo = new NetworkSceneInfo();
-        //if (scene.IsValid)
-        //{
-        //    sceneInfo.AddSceneRef(scene, LoadSceneMode.Additive);
-        //}
-
-        // Start or join (depends on gamemode) a session with a specific name
-        await _runner.StartGame(new StartGameArgs()
+        async void StartGame(GameMode mode)
         {
-            GameMode = mode,
-            SessionName = "TestRoom",
-            //Scene = scene,
-            SceneManager = gameObject.AddComponent<NetworkSceneManagerDefault>()
-        });
-    }
+            // Create the Fusion runner and let it know that we will be providing user input
+            _runner = gameObject.AddComponent<NetworkRunner>();
+            _runner.ProvideInput = true;
 
-    private void HandleShutdown()
-    {
-        SceneManager.LoadScene(0);
-    }
+            // Create the NetworkSceneInfo from the current scene
+            var scene = SceneRef.FromIndex(SceneManager.GetActiveScene().buildIndex);
+            //var sceneInfo = new NetworkSceneInfo();
+            //if (scene.IsValid)
+            //{
+            //    sceneInfo.AddSceneRef(scene, LoadSceneMode.Additive);
+            //}
 
-
-    #region Fusion Callbacks Interface Implementation
-    public void OnConnectedToServer(NetworkRunner runner)
-    {
-        Debug.Log($"{runner.name} connected to server!");
-    }
-
-    public void OnConnectFailed(NetworkRunner runner, NetAddress remoteAddress, NetConnectFailedReason reason)
-    {
-        Debug.Log($"OnConnectFailed due to {reason.ToString()}");
-    }
-
-    public void OnConnectRequest(NetworkRunner runner, NetworkRunnerCallbackArgs.ConnectRequest request, byte[] token)
-    {
-        Debug.Log($"{runner.LocalPlayer.PlayerId} Requested Connection");
-    }
-
-    public void OnCustomAuthenticationResponse(NetworkRunner runner, Dictionary<string, object> data)
-    {
-        Debug.Log($"");
-    }
-
-    public void OnDisconnectedFromServer(NetworkRunner runner, NetDisconnectReason reason)
-    {
-        Debug.Log($"{runner.name} disconnected from server!");
-    }
-
-    public void OnHostMigration(NetworkRunner runner, HostMigrationToken hostMigrationToken)
-    {
-        Debug.Log($"");
-    }
-
-    public void OnInput(NetworkRunner runner, NetworkInput input)
-    {
-        Debug.Log($"OnInput");
-    }
-
-    public void OnInputMissing(NetworkRunner runner, PlayerRef player, NetworkInput input)
-    {
-        Debug.Log($"{player.PlayerId} OnInputMissing");
-    }
-
-    public void OnObjectEnterAOI(NetworkRunner runner, NetworkObject obj, PlayerRef player)
-    {
-        Debug.Log($"");
-    }
-
-    public void OnObjectExitAOI(NetworkRunner runner, NetworkObject obj, PlayerRef player)
-    {
-        Debug.Log($"");
-    }
-
-    public void OnPlayerJoined(NetworkRunner runner, PlayerRef player)
-    {
-        Debug.Log($"{player.PlayerId} joined!");
-
-        if (_runner.IsServer)
-        {
-            // Create a unique position for the player
-            Vector3 spawnPosition = new Vector3((player.RawEncoded % runner.Config.Simulation.PlayerCount) * 3, 1, 0);
-            NetworkObject networkPlayerObject = _runner.Spawn(_playerPrefab, spawnPosition, Quaternion.identity, player);
-            // Keep track of the player avatars for easy access
-            _spawnedCharacters.Add(player, networkPlayerObject);
+            // Start or join (depends on gamemode) a session with a specific name
+            await _runner.StartGame(new StartGameArgs()
+            {
+                GameMode = mode,
+                SessionName = "TestRoom",
+                //Scene = scene,
+                SceneManager = gameObject.AddComponent<NetworkSceneManagerDefault>()
+            });
         }
-    }
 
-    public void OnPlayerLeft(NetworkRunner runner, PlayerRef player)
-    {
-        Debug.Log($"{player.PlayerId} left!");
-
-        if (_spawnedCharacters.TryGetValue(player, out NetworkObject networkObject))
+        private void HandleShutdown()
         {
-            runner.Despawn(networkObject);
-            _spawnedCharacters.Remove(player);
+            SceneManager.LoadScene(0);
         }
-    }
 
-    public void OnReliableDataProgress(NetworkRunner runner, PlayerRef player, ReliableKey key, float progress)
-    {
-        Debug.Log($"");
-    }
 
-    public void OnReliableDataReceived(NetworkRunner runner, PlayerRef player, ReliableKey key, ArraySegment<byte> data)
-    {
-        Debug.Log($"");
-    }
+        #region Fusion Callbacks Interface Implementation
+        public void OnConnectedToServer(NetworkRunner runner)
+        {
+            Debug.Log($"{runner.name} connected to server!");
+        }
 
-    public void OnSceneLoadDone(NetworkRunner runner)
-    {
-        Debug.Log($"OnSceneLoadDone");
-    }
+        public void OnConnectFailed(NetworkRunner runner, NetAddress remoteAddress, NetConnectFailedReason reason)
+        {
+            Debug.Log($"OnConnectFailed due to {reason.ToString()}");
+        }
 
-    public void OnSceneLoadStart(NetworkRunner runner)
-    {
-        Debug.Log($"OnSceneLoadStart");
-    }
+        public void OnConnectRequest(NetworkRunner runner, NetworkRunnerCallbackArgs.ConnectRequest request, byte[] token)
+        {
+            Debug.Log($"{runner.LocalPlayer.PlayerId} Requested Connection");
+        }
 
-    public void OnSessionListUpdated(NetworkRunner runner, List<SessionInfo> sessionList)
-    {
-        Debug.Log($"");
-    }
+        public void OnCustomAuthenticationResponse(NetworkRunner runner, Dictionary<string, object> data)
+        {
+            Debug.Log($"");
+        }
 
-    public void OnShutdown(NetworkRunner runner, ShutdownReason shutdownReason)
-    {
-        Debug.Log($"{shutdownReason.ToString()}");
-        HandleShutdown();
-    }
+        public void OnDisconnectedFromServer(NetworkRunner runner, NetDisconnectReason reason)
+        {
+            Debug.Log($"{runner.name} disconnected from server!");
+        }
 
-    public void OnUserSimulationMessage(NetworkRunner runner, SimulationMessagePtr message)
-    {
-        Debug.Log($"");
+        public void OnHostMigration(NetworkRunner runner, HostMigrationToken hostMigrationToken)
+        {
+            Debug.Log($"");
+        }
+
+        public void OnInput(NetworkRunner runner, NetworkInput input)
+        {
+            Debug.Log($"OnInput");
+        }
+
+        public void OnInputMissing(NetworkRunner runner, PlayerRef player, NetworkInput input)
+        {
+            Debug.Log($"{player.PlayerId} OnInputMissing");
+        }
+
+        public void OnObjectEnterAOI(NetworkRunner runner, NetworkObject obj, PlayerRef player)
+        {
+            Debug.Log($"");
+        }
+
+        public void OnObjectExitAOI(NetworkRunner runner, NetworkObject obj, PlayerRef player)
+        {
+            Debug.Log($"");
+        }
+
+        public void OnPlayerJoined(NetworkRunner runner, PlayerRef player)
+        {
+            Debug.Log($"{player.PlayerId} joined!");
+
+            if (_runner.IsServer)
+            {
+                // Create a unique position for the player
+                Vector3 spawnPosition = new Vector3((player.RawEncoded % runner.Config.Simulation.PlayerCount) * 3, 1, 0);
+                NetworkObject networkPlayerObject = _runner.Spawn(_playerPrefab, spawnPosition, Quaternion.identity, player);
+                // Keep track of the player avatars for easy access
+                _spawnedCharacters.Add(player, networkPlayerObject);
+            }
+        }
+
+        public void OnPlayerLeft(NetworkRunner runner, PlayerRef player)
+        {
+            Debug.Log($"{player.PlayerId} left!");
+
+            if (_spawnedCharacters.TryGetValue(player, out NetworkObject networkObject))
+            {
+                runner.Despawn(networkObject);
+                _spawnedCharacters.Remove(player);
+            }
+        }
+
+        public void OnReliableDataProgress(NetworkRunner runner, PlayerRef player, ReliableKey key, float progress)
+        {
+            Debug.Log($"");
+        }
+
+        public void OnReliableDataReceived(NetworkRunner runner, PlayerRef player, ReliableKey key, ArraySegment<byte> data)
+        {
+            Debug.Log($"");
+        }
+
+        public void OnSceneLoadDone(NetworkRunner runner)
+        {
+            Debug.Log($"OnSceneLoadDone");
+        }
+
+        public void OnSceneLoadStart(NetworkRunner runner)
+        {
+            Debug.Log($"OnSceneLoadStart");
+        }
+
+        public void OnSessionListUpdated(NetworkRunner runner, List<SessionInfo> sessionList)
+        {
+            Debug.Log($"");
+        }
+
+        public void OnShutdown(NetworkRunner runner, ShutdownReason shutdownReason)
+        {
+            Debug.Log($"{shutdownReason.ToString()}");
+            HandleShutdown();
+        }
+
+        public void OnUserSimulationMessage(NetworkRunner runner, SimulationMessagePtr message)
+        {
+            Debug.Log($"");
+        }
+        #endregion  
     }
-    #endregion  
 }
