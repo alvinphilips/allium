5a0d82c712679d5f20657fd1872d9ea22af0b23e
Alvin Philips <alvinphilips257@gmail.com>
2024-02-17 14:35:19 -0500

    Added GameStates

---
+++ Assets/Game/Scripts/Game/States/MainMenuState.cs
@@ -0,0 +1,23 @@
+using Game.Scripts.Patterns;
+using UnityEngine;
+
+namespace Game.Scripts.Game.States
+{
+    public class MainMenuState : IState<GameManager>
+    {
+        public void OnStateEnter(GameManager state)
+        {
+            throw new System.NotImplementedException();
+        }
+        
+        public void OnStateExit(GameManager state)
+        {
+            throw new System.NotImplementedException();
+        }
+        
+        public void OnStateResume(GameManager state)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
