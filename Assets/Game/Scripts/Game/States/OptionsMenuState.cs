663660263920f2ea773f46717b94cdb03b8ed79e
Alvin Philips <alvinphilips257@gmail.com>
2024-02-18 22:08:07 -0500

    fix: Exit from play state now causes timer to stop

--- Assets/Game/Scripts/Game/States/OptionsMenuState.cs
+++ Assets/Game/Scripts/Game/States/OptionsMenuState.cs
@@ -1,18 +1,19 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class OptionsMenuState : MonoBehaviour
+namespace Game.Scripts.Game.States
 {
-    // Start is called before the first frame update
-    void Start()
+    public class OptionsMenuState : MonoBehaviour
     {
+        // Start is called before the first frame update
+        void Start()
+        {
         
-    }
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
+        // Update is called once per frame
+        void Update()
+        {
         
+        }
     }
 }
