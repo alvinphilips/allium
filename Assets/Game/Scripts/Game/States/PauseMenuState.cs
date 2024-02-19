4c81344e9f69f880f5e15d320a5da538cd586a69
Alvin Philips <alvinphilips257@gmail.com>
2024-02-18 22:08:07 -0500

    fix: Exit from play state now causes timer to stop

--- Assets/Game/Scripts/Game/States/PauseMenuState.cs
+++ Assets/Game/Scripts/Game/States/PauseMenuState.cs
@@ -1,31 +1,30 @@
-using Game.Scripts.Game;
 using Game.Scripts.Patterns;
-using System.Collections;
-using System.Collections.Generic;
-using Game.Scripts.Game.States;
 using UnityEngine;
 
-public class PauseMenuState : IState<GameManager>
+namespace Game.Scripts.Game.States
 {
-    public void OnStateEnter(GameManager state)
+    public class PauseMenuState : IState<GameManager>
     {
-        EventBus<GameStates>.Publish(GameStates.Paused);
-        
-        GameManager.Instance.pauseMenu.SetActive(true);
-    }
-
-    public void OnUpdate(GameManager state)
-    {
-        if (Input.GetKeyDown(KeyCode.Escape))
+        public void OnStateEnter(GameManager state)
         {
-            GameManager.Instance.ChangeState(new PlayState());
-        }
-    }
-
-    public void OnStateExit(GameManager state)
-    {
-        EventBus<GameStates>.Publish(GameStates.Running);
+            EventBus<GameStates>.Publish(GameStates.Paused);
         
-        GameManager.Instance.pauseMenu.SetActive(false);
+            GameManager.Instance.pauseMenu.SetActive(true);
+        }
+
+        public void OnUpdate(GameManager state)
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                GameManager.Instance.ChangeState(new PlayState());
+            }
+        }
+
+        public void OnStateExit(GameManager state)
+        {
+            EventBus<GameStates>.Publish(GameStates.Running);
+        
+            GameManager.Instance.pauseMenu.SetActive(false);
+        }
     }
 }
--- Assets/Game/Scripts/Game/States/PlayState.cs
+++ Assets/Game/Scripts/Game/States/PlayState.cs
@@ -1,24 +1,27 @@
-using Game.Scripts.Game;
 using Game.Scripts.Patterns;
-using System.Collections;
-using System.Collections.Generic;
-using Game.Scripts.Game.States;
-using UnityEditor;
 using UnityEngine;
 
-public class PlayState : IState<GameManager>
+namespace Game.Scripts.Game.States
 {
-    public void OnStateEnter(GameManager state)
+    public class PlayState : IState<GameManager>
     {
-        EventBus<GameStates>.Publish(GameStates.Running);
-    }
-    
-    public void OnUpdate(GameManager state) 
-    {
-        Debug.Log("PlayStateUpdating");
-        if(Input.GetKeyDown(KeyCode.Escape))
+        public void OnStateEnter(GameManager state)
         {
-            GameManager.Instance.ChangeState(new PauseMenuState());
+            EventBus<GameStates>.Publish(GameStates.Running);
+        }
+    
+        public void OnUpdate(GameManager state) 
+        {
+            Debug.Log("PlayStateUpdating");
+            if(Input.GetKeyDown(KeyCode.Escape))
+            {
+                GameManager.Instance.ChangeState(new PauseMenuState());
+            }
+        }
+    
+        public void OnStateExit(GameManager state)
+        {
+            EventBus<GameStates>.Publish(GameStates.Paused);
         }
     }
 }
