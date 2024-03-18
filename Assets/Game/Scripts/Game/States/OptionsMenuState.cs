691a989dcb0a1ccf1afe2714f7ca746c0f2b40ab
Alvin Philips <alvinphilips257@gmail.com>
2024-03-17 22:44:02 -0400

    Added Options

--- Assets/Game/Scripts/Game/States/OptionsMenuState.cs
+++ Assets/Game/Scripts/Game/States/OptionsMenuState.cs
@@ -1,19 +1,20 @@
+using Game.Scripts.Patterns;
 using UnityEngine;
 
 namespace Game.Scripts.Game.States
 {
-    public class OptionsMenuState : MonoBehaviour
+    public class OptionsMenuState : IState<GameManager>
     {
-        // Start is called before the first frame update
-        void Start()
+        public void OnStateEnter(GameManager state)
         {
-        
+            GameManager.Instance.optionsMenu.SetActive(true);
+            EventBus<Settings.OptionsMenuState>.Publish(Settings.OptionsMenuState.Show);
         }
-
-        // Update is called once per frame
-        void Update()
-        {
         
+        public void OnStateExit(GameManager state)
+        {
+            EventBus<Settings.OptionsMenuState>.Publish(Settings.OptionsMenuState.Hidden);
+            GameManager.Instance.optionsMenu.SetActive(false);
         }
     }
 }
