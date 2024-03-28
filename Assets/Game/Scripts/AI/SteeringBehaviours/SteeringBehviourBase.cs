627f2b9f92ca1cbfe45f1f68ae391477f708cd22
sami-s-56 <85152176+sami-s-56@users.noreply.github.com>
2024-03-28 03:50:23 -0400

    SteeringBehaviourBaseCode Added

---
+++ Assets/Game/Scripts/AI/SteeringBehaviours/SteeringBehviourBase.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class SteeringBehviourBase : MonoBehaviour
+{
+
+    public float weight = 1f;
+    public Vector3 target = Vector3.zero;
+    public bool bUseMouseInput = true;
+
+    public abstract Vector3 CalculateForce();
+
+    [HideInInspector] public SteeringAgent steeringAgent;
+
+    protected bool bMouseClicked = false;
+
+    protected void CheckMouseInput()
+    {
+        bMouseClicked = false;
+
+        Debug.Log("Checking Mouse Input");
+
+        if(Input.GetMouseButtonDown(0) && bUseMouseInput) 
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if(Physics.Raycast(ray, out hit, 100) )
+            {
+                target = hit.point;
+                bMouseClicked = true;
+            }
+        }
+    }
+
+    protected virtual void OnDrawGizmos()
+    {
+        if(steeringAgent != null)
+        {
+            DebugExtension.DrawArrow(transform.position, target, Color.red);
+            DebugExtension.DrawArrow(transform.position, steeringAgent.velocity, Color.blue);
+        }
+
+        DebugExtension.DrawPoint(target, Color.magenta);
+    }
+}
