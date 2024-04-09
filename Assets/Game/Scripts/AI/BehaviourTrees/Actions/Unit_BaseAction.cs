1fcc0cd25d38d84003501b845434170aac8cb963
sami-s-56 <85152176+sami-s-56@users.noreply.github.com>
2024-04-09 15:53:14 -0400

    Reorganized some code and Added scripts for Behavior Tree Actions

---
+++ Assets/Game/Scripts/AI/BehaviourTrees/Actions/Unit_BaseAction.cs
@@ -0,0 +1,11 @@
+using BehaviorDesigner.Runtime.Tasks;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Unit_BaseAction : Task
+{
+    Unit unit;
+
+
+}
