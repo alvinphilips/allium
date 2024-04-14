e0c134bbb47700e3f77772e9203efad76d7d2f7b
Alvin Philips <alvinphilips257@gmail.com>
2024-04-13 22:13:19 -0400

    basic OptionsMenu stuff

--- Assets/Game/Scripts/UI/OptionsMenu.cs
+++ Assets/Game/Scripts/UI/OptionsMenu.cs
@@ -1,12 +1,28 @@
 ﻿using System.Collections;
+using Game.Scripts.Utils;
+using UnityEngine;
+using UnityEngine.UIElements;
 
 namespace Game.Scripts.UI
 {
     public class OptionsMenu: Menu
     {
+        [SerializeField] private StyleSheet style;
+        
         protected override IEnumerator Generate()
         {
-            throw new System.NotImplementedException();
+            yield return null;
+            
+            var root = Document.rootVisualElement;
+            root.Clear();
+            root.styleSheets.Add(style);
+
+            var container = root.Create("w-full", "h-full", "bg-emerald-600");
+
+            if (IsMobile)
+            {
+                container.AddToClassList("pt-12");
+            }
         }
     }
 }
