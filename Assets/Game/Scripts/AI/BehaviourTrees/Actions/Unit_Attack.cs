6a2c4d1f5fe48821be279fae3d9312f7e7f742e2
sami-s-56 <85152176+sami-s-56@users.noreply.github.com>
2024-04-15 06:09:28 -0400

    Some States Added

--- Assets/Game/Scripts/AI/BehaviourTrees/Actions/Unit_Attack.cs
+++ Assets/Game/Scripts/AI/BehaviourTrees/Actions/Unit_Attack.cs
@@ -1,8 +1,42 @@
+using BehaviorDesigner.Runtime.Tasks;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEngine.UI.GridLayoutGroup;
 
 public class Unit_Attack : Unit_BaseAction
 {
-   
+    public override TaskStatus OnUpdate()
+    {
+        base.OnUpdate();
+
+        if (unit.target != null)
+        {
+            Vector3 vectorToTarget = (unit.target.position - owner.transform.position);
+            Quaternion targetRotation = Quaternion.LookRotation(vectorToTarget);
+            Quaternion rotation = Quaternion.Lerp(unit.transform.rotation, targetRotation, 50f * Time.deltaTime);
+            unit.Aim(rotation);
+
+            float angle = Vector3.Angle(tank.turretTransform.forward, (tank.target.position - owner.transform.position));
+
+            if (Mathf.Abs(angle) < unit.fireThreshould)
+            {
+                if (currentTime > nextFireTime)
+                {
+                    tank.Fire();
+                    nextFireTime = currentTime + tank.fireDelay;
+                }
+            }
+
+            currentTime += Time.deltaTime;
+
+            float distanceToTarget = vectorToTarget.magnitude;
+
+            if (distanceToTarget > tank.range)
+            {
+                fsm.ChangeState(T_ApproachTarget);
+
+            }
+        }
+    }
 }
--- Assets/Game/Scripts/Troops/Unit.cs
+++ Assets/Game/Scripts/Troops/Unit.cs
@@ -1,8 +1,40 @@
+using Game.Scripts;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum UnitType
+{
+    Looters,
+    Footmen
+}
+
 public class Unit : Troop
 {
-    
+    //To decide on which action to perform
+    public UnitType unitType;
+
+    public override Transform GetTarget()
+    {
+        target = PlacementHandler.Instance.GetClosestTarget(transform.position, ObjectType.ResourceBldg, range).transform;
+
+        return target;
+    }
+
+    public override void Aim(Quaternion rotation)
+    {
+        transform.rotation = rotation;
+    }
+
+    public void Loot()
+    {
+        //Action of looting resource to be performed
+    }
+
+    public override void Fire()
+    {
+        Debug.Log("Firing");
+
+        //Firing logic to be placed
+    }
 }
