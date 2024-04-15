f44997cddc022b8c292da88b23c11fbebe630484
Alvin Philips <alvinphilips257@gmail.com>
2024-04-15 00:01:55 -0400

    Silly Things

--- Assets/Game/Scripts/Fusion/Player.cs
+++ Assets/Game/Scripts/Fusion/Player.cs
@@ -1,9 +1,21 @@
 using Fusion;
+using Game.Scripts.Game;
+using Game.Scripts.GameResources;
+using Game.Scripts.Patterns;
 
 namespace Game.Scripts.Fusion
 {
-    public class Player : NetworkBehaviour
+    public class Player : NetworkBehaviour, IVisitor<Resource>
     {
         public string Nickname { get; set; }
+        
+        public void Visit(Resource visitable)
+        {
+            // TODO: Implement
+            if (visitable.Type == ResourceType.Money)
+            {
+                ResourceManager.Instance.AddMoney(visitable.Amount);
+            }
+        }
     }
 }
