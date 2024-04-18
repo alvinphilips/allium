3b708be72e3b2a01689d7fe3b44c77a4f65f59ae
Alvin Philips <alvinphilips257@gmail.com>
2024-04-17 20:58:23 -0400

    Game spawn stuff

--- Assets/Game/Scripts/Game/States/PlayState.cs
+++ Assets/Game/Scripts/Game/States/PlayState.cs
@@ -1,25 +1,57 @@
+using System.Collections.Generic;
+using Game.Scripts.Fusion;
 using Game.Scripts.Patterns;
+using Game.Scripts.UI;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
 
 namespace Game.Scripts.Game.States
 {
     public class PlayState : IState<GameManager>
     {
+        private readonly List<ARRaycastHit> _arHits = new();
+        
         public void OnStateEnter(GameManager state)
         {
             EventBus<GameStates>.Publish(GameStates.Running);
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            MenuManager.Instance.HideDummy();
         }
     
-        public void OnUpdate(GameManager state) 
+        public void OnUpdate(GameManager state)
         {
-            // if(Input.GetKeyDown(KeyCode.Escape))
-            // {
-            //     GameManager.Instance.ChangeState(new PauseMenuState());
-            // }
+            var shouldSpawn = false;
+            var spawnPosition = Vector3.zero;
+            var spawnRotation = Quaternion.identity;
+            
+            if (state.IsAREnabled && Input.GetTouch(0).phase == TouchPhase.Began)
+            {
+                if (!state.arRaycastManager.Raycast(Input.GetTouch(0).position, _arHits, TrackableType.PlaneWithinPolygon))
+                {
+                    return;
+                }
+
+                shouldSpawn = true;
+                spawnPosition = _arHits[0].pose.position;
+                spawnRotation = _arHits[0].pose.rotation;
+
+                foreach (var plane in state.arPlaneManager.trackables)
+                {
+                    plane.gameObject.SetActive(false);
+                }
+
+                state.arPlaneManager.enabled = false;
+            } else if (Input.GetMouseButtonDown(0))
+            {
+                var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            }
+            
+            if (!shouldSpawn) return;
+            
+            state.SpawnItem(spawnPosition, spawnRotation);
         }
-    
+        
         public void OnStateExit(GameManager state)
         {
             EventBus<GameStates>.Publish(GameStates.Paused);
